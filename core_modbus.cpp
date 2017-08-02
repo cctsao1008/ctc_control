72be4b3c5a1ed134d9051162cc6d3d4fd804090b
tsao.ricardo <tsao.ricardo@iac.com.tw>
2017-08-02 16:17:47 +0800

    add files.
    Signed-off-by: tsao.ricardo <tsao.ricardo@iac.com.tw>

---
+++ core_modbus.cpp
@@ -0,0 +1,16 @@
+/**
+* @file core_modbus.cpp
+*
+* core_modbus
+*
+*   core_modbus.
+*
+* @author Ricardo <tsao.ricardo@iac.com.tw>
+*/
+
+#include "core_common.h"
+
+int core_mqtt_open(void)
+{
+	return 0;
+}
---
+++ core_modbus.h
@@ -0,0 +1,11 @@
+/**
+* @file core_modbus.h
+*
+* core_modbus
+*
+*   core_modbus.
+*
+* @author Ricardo <tsao.ricardo@iac.com.tw>
+*/
+
+#pragma once
