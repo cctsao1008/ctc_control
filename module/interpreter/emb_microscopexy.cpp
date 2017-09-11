74e294139b2b79a24b724ec3ec8f47c9320d4783
Joshua <jan.joshua@iac.com.tw>
2017-09-11 10:05:48 +0800

    Update emb files

---
+++ module/interpreter/emb_microscopexy.cpp
@@ -0,0 +1,9 @@
+/**********************************************
+ * @file rs485p2.h                  	      *
+ *                                            *
+ * RS485 Port2 Handle Thread				  *
+ *                                            *
+ * @author Joshua <jan.joshua@iac.com.tw>     *
+ **********************************************/
+
+#include "core_common.h"
